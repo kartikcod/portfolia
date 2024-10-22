55edbab62aa39ee1326e5f21dd94935a66001ca6
kartik mahato <118644047+kartikcod@users.noreply.github.com>
2024-10-22 21:34:43 +0530

    hero section

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import background from "E:/React project/portfolia/background/bg.svg";
+import img from "E:/React project/portfolia/public/image/kartik.png";
 
 export const Hero = () => {
   return (
     <>
-    <div >
-        <img src={background} alt="bg image" /> 
+      <div className="flex flex-row items-center w-5/5 -my-32 justify-center  text-center">
         <div>
-            <div>
-
-            </div>
-            <div>
-                
-            </div>
+          <span className="text-white text-8xl font-sans font-bold my-16">
+            I'm Frontend Developer
+          </span>
+          <div className="text-white mt-14 text-center">
+            I build responsive, easy-to-use web <br /> interfaces with React.js,
+            Material UI, and Tailwind CSS. I'm skilled <br />
+            in Git and GitHub for smooth teamwork. My goal is to create <br />
+            clean designs that improve user experience and meet project needs.
+          </div>
         </div>
-    </div>
-     
+        <span className=" w-3/6 scale-150">
+          <img src={img} alt="kartik img" />
+        </span>
+      </div>
     </>
   );
 };
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export const Navbar = () => {
   return (
     <>
-    <nav>
-        <div className='flex justify-center items-center content-center py-3 '>
-            <div className='bg-white w-96 h-14 rounded-xl border-black '>
-                <ul className='flex justify-center gap-8  py-4'>
-                    <li className='text-white font-medium hover:text-xl transition-all '>Portfolia</li>
-                    <li className='text-white font-medium hover:text-xl transition-all'>Experiance</li>
-                    <li className='text-white font-medium  hover:text-xl transition-all'>Contact Us</li>
-                    <li className='text-white font-medium hover:text-xl transition-all'>Skill</li>
+    <nav >
+        <div className='flex justify-center items-center content-center py-8 '>
+            <div className='bg-black w-96 h-auto rounded-xl '>
+                <ul className='flex justify-center gap-8 border rounded-xl py-4'>
+                    <li className='text-white font-medium cursor-pointer hover:text-cyan-300 transition-all'>Portfolia</li>
+                    <li className='text-white font-medium cursor-pointer hover:text-cyan-300 transition-all'>Experiance</li>
+                    <li className='text-white font-medium cursor-pointer hover:text-cyan-300 transition-all'>Contact Us</li>
+                    <li className='text-white font-medium cursor-pointer hover:text-cyan-300 transition-all'>Skill</li>
                 </ul>
             </div>
         </div>
